08da55209ecbe3b48984ab343d90914f1c088979
James Andrews <jbandrew14@gmail.com>
2021-11-06 11:23:18 -0400

    content is being displayed (but it's not very pretty)

--- django_music/urls.py
+++ django_music/urls.py
@@ -17,8 +17,10 @@ import debug_toolbar
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
+from vinyl_lovers import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('__debug__/', include(debug_toolbar.urls))
+    path('__debug__/', include(debug_toolbar.urls)),
+    path('', views.list_albums, name="album-entries"),
 ]
--- vinyl_lovers/views.py
+++ vinyl_lovers/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Album
 
 # Create your views here.
+
+
+def list_albums(request):
+    albums = Album.objects.all()
+    return render(request, "vinyl_lovers/list_albums.html", {"albums": albums})
