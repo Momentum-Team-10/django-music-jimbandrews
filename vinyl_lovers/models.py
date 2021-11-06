d6596c0a93de5211d1f346a1d9893b09c10baad2
James Andrews <jbandrew14@gmail.com>
2021-11-06 09:35:32 -0400

    added templates and .env (might come back to that later), and created Album model in models.py

--- vinyl_lovers/models.py
+++ vinyl_lovers/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=500)
+    artist = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    orignal_release = models.DateField()
+
+    MINT = 'M'
+    NEAR_MINT = 'NM'
+    VERY_GOOD_PLUS = 'VG+'
+    VERY_GOOD = 'VG'
+    GOOD_PLUS = 'G+'
+    GOOD = 'G'
+    FAIR = 'F'
+    POOR = 'P'
+    CONDITION_CHOICES = [
+        (MINT, 'Mint'),
+        (NEAR_MINT, 'Near Mint'),
+        (VERY_GOOD_PLUS, 'Very Good Plus'),
+        (VERY_GOOD, 'Very Good'),
+        (GOOD_PLUS, 'Good Plus'),
+        (GOOD, 'Good'),
+        (FAIR, 'Fair'),
+        (POOR, 'Poor'),
+    ]
+    condition = models.CharField(
+        max_length=3,
+        choices=CONDITION_CHOICES,
+        default=MINT,
+    )
