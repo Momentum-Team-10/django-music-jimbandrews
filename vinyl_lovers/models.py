9335952606310ea9e7b2861dd9f45d2b0c79dc34
James Andrews <jbandrew14@gmail.com>
2021-11-08 11:00:06 -0500

    adding User class to models.py to allow more users beyond admin to be added

--- vinyl_lovers/models.py
+++ vinyl_lovers/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
+class User(AbstractUser):
+    def __repr__(self):
+        return f"<User username={self.username}>"
+
+    def __str__(self):
+        return self.username
+
+
 class Album(models.Model):
     title = models.CharField(max_length=500)
     artist = models.CharField(max_length=500)
